d2971c9a2fe6524fb59de427bcec680e216a2b7a
Tuba282 <tubajan282@gmail.com>
2025-09-18 14:05:41 +0500

    adjest number tricker

--- src/Components/NumberTricker.jsx
+++ src/Components/NumberTricker.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { db, doc, getDoc, setDoc, updateDoc } from '../Settings/firebaseConfig';
 import { NumberTicker } from "../Components/magicui/number-ticker";
+import { FaBullseye } from "react-icons/fa";
+import { FaThList } from "react-icons/fa";
+import { IoShieldCheckmark } from "react-icons/io5";
 
 const NumberTricker = () => {
     const [count, setCount] = useState(0);
@@ -28,32 +31,35 @@ const NumberTricker = () => {
         return () => { isMounted = false; };
     }, []);
     return (
-        <div className="relative flex items-center flex-wrap gap-10 justify-around bg-fixed bg-[url(https://images.unsplash.com/photo-1521247205284-f4f3c95fdb5a?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8)] bg-no-repeat bg-center bg-cover h-96 text-2xl font-bold">
-            <div className="absolute top-0 left-0 bg-black/80 -z-1 w-full h-full"></div>
-            <div className="grid justify-center items-center gap-4">
-                <span className='text-5xl text-center text-white font-[Quicksand]'>Projects</span>
-                <span className="whitespace-pre-wrap text-8xl  font-[Quicksand] text-center font-medium tracking-tighter text-white">
+        <div className="relative overflow-hidden flex items-center flex-wrap gap-0 md:gap-10 justify-evenly bg-fixed bg-[url(https://images.unsplash.com/photo-1521247205284-f4f3c95fdb5a?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8)] bg-no-repeat bg-center bg-cover h-40 py-5 sm:p-0 md:h-96 text-2xl font-bold">
+            <div className="absolute top-0 left-0 bg-black -z-1 w-full h-full"></div>
+            <div className="flex flex-col justify-center items-center gap-2 sm:gap-4">
+                <FaThList className='text-white xl:text-8xl text-center' />
+                <span className='text-lg sm:text-2xl  text-center text-white font-[Quicksand]'>Projects</span>
+                <span className="whitespace-pre-wrap text-xl sm:text-4xl font-[Quicksand] text-center font-medium tracking-tighter text-white">
                     <NumberTicker
                         value={31}
-                        className="whitespace-pre-wrap  font-[Quicksand] text-8xl text-center font-medium tracking-tighter text-white"
+                        className="whitespace-pre-wrap  text-xl sm:text-4xl  font-[Quicksand] text-center font-medium tracking-tighter text-white"
                     />+
                 </span>
             </div>
-            <div className="grid justify-center items-center gap-4">
-                <span className='text-5xl text-center text-white font-[Quicksand]'>Linkedin <br /> connections</span>
-                <span className="whitespace-pre-wrap text-8xl  font-[Quicksand] text-center font-medium tracking-tighter text-white">
+            <div className="flex flex-col justify-center items-center gap-2 sm:gap-4">
+                <IoShieldCheckmark className='text-white xl:text-8xl text-center' />
+                <span className='text-lg sm:text-2xl  text-center text-white font-[Quicksand]'>Linkedin <br /> connections</span>
+                <span className="whitespace-pre-wrap text-xl sm:text-4xl font-[Quicksand] text-center font-medium tracking-tighter text-white">
                     <NumberTicker
                         value={1345}
-                        className="whitespace-pre-wrap  font-[Quicksand] text-8xl text-center font-medium tracking-tighter text-white"
+                        className="whitespace-pre-wrap  text-xl sm:text-4xl  font-[Quicksand] text-center font-medium tracking-tighter text-white"
                     />+
                 </span>
             </div>
-            <div className="grid justify-center items-center gap-4">
-                <span className='text-5xl text-center text-white font-[Quicksand]'>Portfolio <br /> Visitors</span>
-                <span className="whitespace-pre-wrap text-8xl  font-[Quicksand] text-center font-medium tracking-tighter text-white">
+            <div className="flex flex-col justify-center items-center gap-2 sm:gap-4">
+                <FaBullseye className='text-white xl:text-8xl text-center' />
+                <span className='text-lg sm:text-2xl  text-center text-white font-[Quicksand]'>Portfolio <br /> Visitors</span>
+                <span className="whitespace-pre-wrap text-xl sm:text-4xl font-[Quicksand] text-center font-medium tracking-tighter text-white">
                     <NumberTicker
                         value={count || 48}
-                        className="whitespace-pre-wrap  font-[Quicksand] text-8xl text-center font-medium tracking-tighter text-white"
+                        className="whitespace-pre-wrap  text-xl sm:text-4xl  font-[Quicksand] text-center font-medium tracking-tighter text-white"
                     />+
                 </span>
             </div>
